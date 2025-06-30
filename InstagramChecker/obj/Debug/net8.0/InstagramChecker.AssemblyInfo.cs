202ea1e1ec606de8f9fc86ed00afcb40dfa7bb03
son.dq1 <daoquangsonlc98@gmail.com>
2025-06-30 18:34:38 +0700

    Add Dockerfile for Render deploy

--- InstagramChecker/obj/Debug/net8.0/InstagramChecker.AssemblyInfo.cs
+++ InstagramChecker/obj/Debug/net8.0/InstagramChecker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InstagramChecker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e73f9ccb89791fa83c6809793a323ffe3ef6b3a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("InstagramChecker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InstagramChecker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
